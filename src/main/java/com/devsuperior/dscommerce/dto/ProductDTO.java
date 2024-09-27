3e5121dc699ef2aa0f82a9d44dea83b2d9e3725b
elcio2014 <elcio.gecont@gmail.com>
2024-09-27 01:38:31 -0300

    GET product by id

---
+++ src/main/java/com/devsuperior/dscommerce/dto/ProductDTO.java
@@ -0,0 +1,55 @@
+package com.devsuperior.dscommerce.dto;
+
+import com.devsuperior.dscommerce.entities.Product;
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+public class ProductDTO {
+
+    private Long id;
+    private String name;
+    private String description;
+    private Double price;
+    private String imgUrl;
+
+    public ProductDTO() {
+    }
+
+    public ProductDTO(Long id, String description, String name, Double price, String imgUrl) {
+        this.id = id;
+        this.description = description;
+        this.name = name;
+        this.price = price;
+        this.imgUrl = imgUrl;
+    }
+
+    public ProductDTO(Product entity) {
+        id = entity.getId();
+        description = entity.getDescription();
+        name = entity.getName();
+        price = entity.getPrice();
+        imgUrl = entity.getImgUrl();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+}
